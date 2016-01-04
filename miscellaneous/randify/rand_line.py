3b52f06079ade79db1a1f4ca34c340a3a1efafb8
Bill Westfall <billwestfall@yahoo.com>
2016-01-03 21:24:10 -0800

    Update rand_line.py

--- miscellaneous/randify/rand_line.py
+++ miscellaneous/randify/rand_line.py
@@ -3,6 +3,6 @@
 import random
 import sys
 import subprocess
-rand_word=(random.choice(list(open('/Users/billw/Documents/web2'))))
+rand_word=(random.choice(list(open('/usr/share/dict/words'))))
 f1=open('./rand_stuff.txt', 'w+')
 f1.write(rand_word)
