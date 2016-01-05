7f7105a76d3597b7969c1f9627391c622465deab
Bill Westfall <billwestfall@yahoo.com>
2016-01-04 21:46:03 -0800

    Update wrapper.sh
    add random tail vs head and also status after four seconds

--- miscellaneous/randify/wrapper.sh
+++ miscellaneous/randify/wrapper.sh
@@ -3,5 +3,14 @@
 rm -f rand_stuff.txt
 python rand_line.py
 cat rand_stuff.txt
-RANDSHELL=`head -c 6 rand_stuff.txt`
+unset RAND_NUM
+unset RANDSHELL
+RAND_NUM=$((( RANDOM % 2 ) + 1 ))
+if [ "$RAND_NUM" = 2 ]; then
+RANDSHELL=`head -c 5 rand_stuff.txt`
+else
+RANDSHELL=`tail -c 5 rand_stuff.txt`
+fi
 ./spotify play $RANDSHELL
+sleep 4
+./spotify status
