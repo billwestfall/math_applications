bbe3dfc0a16d1d256aa271e4d3b0e06518dd4aab
Bill Westfall <billwestfall@yahoo.com>
2016-01-05 19:41:02 -0800

    Update wrapper.sh
    add random chance to play album to script

--- miscellaneous/randify/wrapper.sh
+++ miscellaneous/randify/wrapper.sh
@@ -5,12 +5,19 @@ python rand_line.py
 cat rand_stuff.txt
 unset RAND_NUM
 unset RANDSHELL
+# add in random chance to take characters from head or tail of term
 RAND_NUM=$((( RANDOM % 2 ) + 1 ))
 if [ "$RAND_NUM" = 2 ]; then
 RANDSHELL=`head -c 5 rand_stuff.txt`
 else
 RANDSHELL=`tail -c 5 rand_stuff.txt`
 fi
+# add in one out of twenty chance to play whole album
+RAND_ALB=$((( RANDOM % 20 )))
+if [ "$RAND_ALB" = 15 ]; then
+./spotify play album $RANDSHELL
+else
 ./spotify play $RANDSHELL
-sleep 4
+fi
+sleep 2
 ./spotify status
