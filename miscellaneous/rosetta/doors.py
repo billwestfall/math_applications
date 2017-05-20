a367f22cbd264776aa9909be1dd8902b1e639064
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 11:26:53 -0700

    Update doors.py

--- miscellaneous/rosetta/doors.py
+++ miscellaneous/rosetta/doors.py
@@ -5,6 +5,7 @@ import random
 doors = 100
 prize = (random.randint(1,100))
 while (doors > 0):
-   if doors == prize
+   if doors == prize:
    print ("The prize is begind door "), print(prize)
+   else
    doors = doors - 1
