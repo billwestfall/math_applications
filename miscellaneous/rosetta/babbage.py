14dd878d355fb080225577f62a5c36938d3b2eb1
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 20:32:30 -0700

    Update babbage.py

--- miscellaneous/rosetta/babbage.py
+++ miscellaneous/rosetta/babbage.py
@@ -5,6 +5,5 @@ babbage = i * i
 while (i > 0):
    if str(babbage).find("269696") == 1:
       print ("The Babbage number is "), print(i), print(" and the root is "), print(babbage)
-      break
    else:
       i = i + 1
