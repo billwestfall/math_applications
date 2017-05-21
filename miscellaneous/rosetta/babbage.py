441135ee909b20d36a86dd04cccbaaec9624c996
Bill Westfall <billwestfall@yahoo.com>
2017-05-20 19:58:51 -0700

    Create babbage.py

---
+++ miscellaneous/rosetta/babbage.py
@@ -0,0 +1,12 @@
+# Babbage problem in Python
+
+import random
+
+doors = 100
+prize = (random.randint(1,100))
+while (doors > 0):
+   if doors == prize:
+      print ("The prize is begind door "), print(prize)
+      break
+   else:
+doors = doors - 1
