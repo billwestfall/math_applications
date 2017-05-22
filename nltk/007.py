e69d51f2f28926159ba67fd16049d1a587d4dee7
Bill Westfall <billwestfall@yahoo.com>
2017-05-22 13:38:30 -0700

    Create 007.py
    from http://www.nltk.org/howto/generate.html

---
+++ nltk/007.py
@@ -0,0 +1,4 @@
+from nltk.parse.generate import generate, demo_grammar
+from nltk import CFG
+grammar = CFG.fromstring(demo_grammar)
+for sentence in generate(grammar, n=10): print(' '.join(sentence))
