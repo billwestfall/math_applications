c8dcbe1ccf4fdcd8d95318bed324d2df8836305a
Bill Westfall <billwestfall@yahoo.com>
2017-05-24 14:12:53 -0700

    Update random_32.py

--- miscellaneous/rosetta/random_32.py
+++ miscellaneous/rosetta/random_32.py
@@ -1,6 +1,6 @@
-# my take on the 100 doors challege in Python
+# random 32 bit number in Python
 
 import random
 
-my_rand = (random.randint(0,100))
+my_rand = (random.randint(0,2147483647))
 print(my_rand)
