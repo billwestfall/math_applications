3247f40814a0bec982bf3eaa2ea6c9a0cba9f5d8
Bill Westfall <billwestfall@yahoo.com>
2017-05-24 14:09:19 -0700

    Create random_32.py

---
+++ miscellaneous/rosetta/random_32.py
@@ -0,0 +1,12 @@
+# my take on the 100 doors challege in Python
+
+import random
+
+doors = 100
+prize = (random.randint(1,100))
+while (doors > 0):
+   if doors == prize:
+      print ("The prize is begind door "), print(prize)
+      break
+   else:
+doors = doors - 1
