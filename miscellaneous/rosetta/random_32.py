bea0120f3cddd17d4b8321eafcd99c29bf8c1b8a
Bill Westfall <billwestfall@yahoo.com>
2017-05-24 14:11:29 -0700

    Update random_32.py

--- miscellaneous/rosetta/random_32.py
+++ miscellaneous/rosetta/random_32.py
@@ -2,11 +2,5 @@
 
 import random
 
-doors = 100
-prize = (random.randint(1,100))
-while (doors > 0):
-   if doors == prize:
-      print ("The prize is begind door "), print(prize)
-      break
-   else:
-doors = doors - 1
+my_rand = (random.randint(0,100))
+print(my_rand)
