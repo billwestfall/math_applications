f1fe8620046c3137a39e2b7d659d98e266145d1e
Bill Westfall <billwestfall@yahoo.com>
2017-05-25 17:40:12 -0700

    Update collatz.py

--- miscellaneous/rosetta/collatz.py
+++ miscellaneous/rosetta/collatz.py
@@ -3,8 +3,3 @@
 my_number = raw_input('Enter a positive integer: ')
 while (doors != 1):
    if my_number % 2 == 0:
-      $my_number = $my_number / 2
-      puts $my_number
-   else:
-      $my_number = (3 * $my_number) + 1
-      puts $my_number
