fad316845088c95ed4439109e583e23a349a3545
Bill Westfall <billwestfall@yahoo.com>
2017-05-25 17:47:41 -0700

    Update collatz.py

--- miscellaneous/rosetta/collatz.py
+++ miscellaneous/rosetta/collatz.py
@@ -1,6 +1,7 @@
 # collatz number in Python
 
-my_number = input('Enter a positive integer: ')
+my_no = input('Enter a positive integer: ')
+my_number = int(my_no)
 while (my_number != 1):
    if my_number % 2 == 0:
       my_number = my_number / 2
