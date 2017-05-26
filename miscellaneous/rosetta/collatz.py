b81b76f49289c357d9a8bd525a381b42e2c5330e
Bill Westfall <billwestfall@yahoo.com>
2017-05-25 17:34:09 -0700

    Update collatz.py

--- miscellaneous/rosetta/collatz.py
+++ miscellaneous/rosetta/collatz.py
@@ -1,1 +1,10 @@
 # collatz number in Python
+
+doors = 100
+prize = (random.randint(1,100))
+while (doors > 0):
+   if doors == prize:
+      print ("The prize is begind door "), print(prize)
+      break
+   else:
+doors = doors - 1
