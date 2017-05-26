c7cd0ee0a7730f8bf6fd641a0e63cc1af32345b4
Bill Westfall <billwestfall@yahoo.com>
2017-05-25 17:50:30 -0700

    Update collatz.py

--- miscellaneous/rosetta/collatz.py
+++ miscellaneous/rosetta/collatz.py
@@ -5,7 +5,9 @@ my_number = int(my_no)
 while (my_number != 1):
    if my_number % 2 == 0:
       my_number = my_number / 2
-      print int(my_number)
+      my_val = int(my_number)
+      print(my_val)
    else:
       my_number = (3 * my_number) + 1
-      print int(my_number)
+      my_val = int(my_number)
+      print(my_val)
