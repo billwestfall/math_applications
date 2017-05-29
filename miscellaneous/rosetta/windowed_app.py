2af3ce71eaaa4bb34a737e42b0582b597d17bd0f
Bill Westfall <billwestfall@yahoo.com>
2017-05-28 20:06:26 -0700

    Update windowed_app.py

--- miscellaneous/rosetta/windowed_app.py
+++ miscellaneous/rosetta/windowed_app.py
@@ -3,9 +3,9 @@ from tkinter import *
 master = Tk()
 
 def callback():
-    print("click me")
+    print("There have been no clicks yet")
 
-b = Button(master, text="OK", command=callback)
+b = Button(master, text="click me", command=callback)
 b.pack()
 
 mainloop()
