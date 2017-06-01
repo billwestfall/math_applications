88a4aaa30fa9af3b5c861dca0afbe6f59e8b2482
Bill Westfall <billwestfall@yahoo.com>
2017-06-01 13:17:32 -0700

    Create munge.py

---
+++ dataviz/chapt02/munge.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+
+#A
+student_data = [ {'name': 'Bob', 'id':0, 'scores':[68, 7, 5, 56, 81]}, {'name': 'Alice', 'id':1, 'scores':[75, 90, 64, 88]}, {'name': 'Carol', 'id':2, 'scores':[59, 74, 71, 68]}, {'name': 'Dan', 'id':3, 'scores':[64, 58, 53, 62]},]
