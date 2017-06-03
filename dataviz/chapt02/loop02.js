365b43713902b281806872cd1076a3fa138e1e0c
Bill Westfall <billwestfall@yahoo.com>
2017-06-03 12:24:14 -0700

    Update loop02.js

--- dataviz/chapt02/loop02.js
+++ dataviz/chapt02/loop02.js
@@ -1,6 +1,6 @@
 var obj = {a:3, b:2, c:4};
 for (var prop in obj) {
   if( obj.hasOwnProperty( prop ) ) {
-    process.stdout.write("o. " + prop + " = " obj[prop]);
+    process.stdout.write("o. " + prop + ' = ' obj[prop]);
   }
 }
