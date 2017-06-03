e364c11c32b4085b24acfaca6e9564c21917ebec
billw@concur.com <billw@concur.com>
2017-06-03 11:15:37 -0700

    Correct spacing

--- dataviz/chapt02/munge.py
+++ dataviz/chapt02/munge.py
@@ -5,27 +5,26 @@ student_data = [ {'name': 'Bob', 'id':0, 'scores':[68, 7, 5, 56, 81]}, {'name': 
 
 #B
 def process_student_data(data, pass_threshold=60, merit_threshold=75): """ Perform some basic stats on some student data. """
-  
+
 #C
-for sdata in data:
-  
-  av = sum(sdata['scores'])/float(len(sdata['scores']))
-  sdata['average'] = av
-  
-  if av > merit_threshold:
-    
-    sdata['assessment'] = 'passed with merit'
-  elif av > pass_threshold:
-    
-    sdata['assessment'] = 'passed'
-  else:
-    
-    sdata['assessment'] = 'failed'
-    
-    #D
-    print("%s's (id: %d) final assessment is %s"%(
-      sdata['name'], sdata['id'], sdata['assessment'].upper()))
-    
+    for sdata in data:
+
+        av = sum(sdata['scores'])/float(len(sdata['scores']))
+        sdata['average'] = av
+
+        if av > merit_threshold:
+            sdata['assessment'] = 'passed with merit'
+
+        elif av > pass_threshold:
+            sdata['assessment'] = 'passed'
+
+        else:
+            sdata['assessment'] = 'failed'
+
+        #D
+        print("%s's (id: %d) final assessment is %s"%(
+        sdata['name'], sdata['id'], sdata['assessment'].upper()))
+
     #E
     if __name__ == '__main__':
       process_student_data(student_data)
