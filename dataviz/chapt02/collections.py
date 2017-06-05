4586e169fca4b5a1f39664945b5c34b5becf7fe6
Bill Westfall <billwestfall@yahoo.com>
2017-06-05 14:09:25 -0700

    Create collections.py

---
+++ dataviz/chapt02/collections.py
@@ -0,0 +1,17 @@
+from collections import Counter, defaultdict, OrderedDict
+
+items = ['F', 'C', 'C', 'A', 'B', 'A', 'C', 'E', 'F']
+
+cntr = Counter(items)
+print(cntr)
+cntr('C') -=1
+print(cntr)
+
+d = defaultdict(int)
+
+for item in items:
+    d[item] += 1
+
+d
+
+OrderedDict(sorted(d.items(), key=lambda i: i[1]))
