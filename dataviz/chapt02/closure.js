daee8183d3c30821e08b7f854fcee2c1cfc5fa45
Bill Westfall <billwestfall@yahoo.com>
2017-06-05 14:51:31 -0700

    Create closure.js

---
+++ dataviz/chapt02/closure.js
@@ -0,0 +1,11 @@
+function Counter(inc) {
+  var count = 0;
+  var add = function() {
+    count += inc;
+    console.log('Current count: ' + count);
+  }
+  return add;
+}
+
+var inc2 = Counter(2);
+inc2();
