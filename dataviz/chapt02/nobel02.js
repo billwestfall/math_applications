a5b9243cbb3305cb87c57b8ac5469a3778adc806
Bill Westfall <billwestfall@yahoo.com>
2017-06-04 21:25:57 -0700

    Create nobel02.js

---
+++ dataviz/chapt02/nobel02.js
@@ -0,0 +1,23 @@
+var Citizen = {
+  setCitizen: function(name, country){
+    this.name = name;
+    this.country = country;
+    return this;
+  },
+  printDetails: function(){
+    console.log('Citizen' + this.name + ' from ', + this.country);
+  }
+};
+
+var Winner = Object.create(Citizen);
+
+Winner.setWinner = function(name, country, category, year){
+  this.setCitizen(name, country);
+  this.category = category;
+  this.year = year;
+  return this;
+};
+
+Winner.printDetails = function(){
+  console.log('Nobel winner ' + this.name + ' from ' + this.country + ', category ' + this.category )
+}
