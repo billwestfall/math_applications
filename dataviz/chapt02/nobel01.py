f03507ab6714ccbac7fa530482f704f1c9833d3a
Bill Westfall <billwestfall@yahoo.com>
2017-06-04 20:08:37 -0700

    Create nobel01.py

---
+++ dataviz/chapt02/nobel01.py
@@ -0,0 +1,11 @@
+class Winner(Citizen):
+
+  def __init__(self, name, country, category, year):
+      super(Winner, self).__init__(name, country)
+      self.category = category
+      self.year = year
+  def print_details(self):
+      print('Winner %s from %s, category %s, year %s' %(self.name, self.country, self.category, str(self.year)))
+      
+w = Winner('Albert E.', 'Switzerland', 'Physics', 1921)
+w.print_details()
