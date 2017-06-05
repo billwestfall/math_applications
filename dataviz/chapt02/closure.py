b03f028306d2491cb96ad7e63161d183ff978d20
Bill Westfall <billwestfall@yahoo.com>
2017-06-05 15:06:53 -0700

    Update closure.py

--- dataviz/chapt02/closure.py
+++ dataviz/chapt02/closure.py
@@ -1,6 +1,9 @@
 def get_counter(inc):
     count = 0
     def add():
+        nonlocal count
         count += inc
         print('Current count: ' + str(count))
         return add
+cntr = get_counter(2)
+cntr()
