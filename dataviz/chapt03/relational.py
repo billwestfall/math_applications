6dafa72d6007e59de275713b2eb5d6bce25e9472
Bill Westfall <billwestfall@yahoo.com>
2017-06-06 14:17:35 -0700

    Create relational.py

---
+++ dataviz/chapt03/relational.py
@@ -0,0 +1,14 @@
+engine = create_engine('sqlite:///data/nobel_prize.db', echo=True)
+
+class Winner(Base):
+    __tablename__ = 'winners'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category = Column(String)
+    year = Column(Integer)
+    nationality = Column(String)
+    sex = Column(Enum('male', 'female'))
+
+    def __repr__(self):
+        return "<Winner(name='%s', category='%s', year='%s')>" %(self.name, self.category, self.year)
