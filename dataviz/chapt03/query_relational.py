ee233edd9d9690b6ed962df9228d00284a1f0909
Bill Westfall <billwestfall@yahoo.com>
2017-06-06 15:39:09 -0700

    Update query_relational.py

--- dataviz/chapt03/query_relational.py
+++ dataviz/chapt03/query_relational.py
@@ -1,23 +1,4 @@
-nobel_winners = [
-  {'category': 'Physics',
-   'name': 'Albert Einstein',
-   'nationality': 'Swiss',
-   'sex': 'male',
-   'year': 1921
- },
- {'category': 'Physics',
-  'name': 'Paul Dirac',
-  'nationality': 'British',
-  'sex': 'male',
-  'year': 1933
-},
-{'category': 'Chemistry',
- 'name': 'Marie Curie',
- 'nationality': 'Polish',
- 'sex': 'female',
- 'year': 1911
-}
-]
+
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Enum
@@ -29,19 +10,6 @@ engine = create_engine('sqlite:///nobel_prize.db', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
-
-class Winner(Base):
-    __tablename__ = 'winners'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    category = Column(String)
-    year = Column(Integer)
-    nationality = Column(String)
-    sex = Column(Enum('male', 'female'))
-
-    def __repr__(self):
-        return "<Winner(name='%s', category='%s', year='%s')>" %(self.name, self.category, self.year)
 
 session.new
 session.query(Winner).count()
