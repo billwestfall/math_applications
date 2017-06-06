157d16f70ae67f0dbdba6be891a8369072945cbb
Bill Westfall <billwestfall@yahoo.com>
2017-06-06 15:40:16 -0700

    Update query_relational.py

--- dataviz/chapt03/query_relational.py
+++ dataviz/chapt03/query_relational.py
@@ -11,5 +11,18 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
+class Winner(Base):
+    __tablename__ = 'winners'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category = Column(String)
+    year = Column(Integer)
+    nationality = Column(String)
+    sex = Column(Enum('male', 'female'))
+
+    def __repr__(self):
+        return "<Winner(name='%s', category='%s', year='%s")>" %(self.name, self.category, self.year)
+
 session.new
 session.query(Winner).count()
