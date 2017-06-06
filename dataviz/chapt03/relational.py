7fa27ec7b12cb6bb3d353750b97863139f3bbbaa
Bill Westfall <billwestfall@yahoo.com>
2017-06-06 14:55:42 -0700

    Update relational.py

--- dataviz/chapt03/relational.py
+++ dataviz/chapt03/relational.py
@@ -1,10 +1,34 @@
+nobel_winners = [
+  {'category': 'Physics',
+   'name': 'Albert Einstein',
+   'nationality': 'Swiss',
+   'sex': 'male',
+   'year': 1921
+ },
+ {'category': 'Physics',
+  'name': 'Paul Dirac',
+  'nationality': 'British',
+  'sex': 'male',
+  'year': 1933
+},
+{'category': 'Chemistry',
+ 'name': 'Marie Curie',
+ 'nationality': 'Polish',
+ 'sex': 'female',
+ 'year': 1911
+}
+]
+
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
-engine = create_engine('sqlite:///nobel_prize.db', echo=True)
+engine = create_engine('sqlite:///data/nobel_prize.db', echo=True)
 
 class Winner(Base):
     __tablename__ = 'winners'
@@ -20,3 +44,7 @@ class Winner(Base):
         return "<Winner(name='%s', category='%s', year='%s')>" %(self.name, self.category, self.year)
 
 Base.metadata.create_all(engine)
+
+winner_rows = [Winner(**w) for w in nobel_winners]
+session.add_all(winner_rows)
+session.commit()
