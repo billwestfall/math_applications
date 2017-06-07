5dc9b81015bb8ad0abccf21c06f5084c370bdb1f
Bill Westfall <billwestfall@yahoo.com>
2017-06-07 14:15:23 -0700

    Update dataset_create.py

--- dataviz/chapt03/dataset_create.py
+++ dataviz/chapt03/dataset_create.py
@@ -1,6 +1,7 @@
 nobel_winners = [{"category": "Physics","name":"Albert Einstein","nationality":"Swiss","sex":"male","year":1921},{"category":"Physics","name":"PaulDirac","nationality":"British","sex":"male","year":1933},{"category":"Chemistry","name":"Marie Curie","nationality":"Polish","sex":"female","year":1911}]
 
 import dataset
+from dataset import connect
 
 db = dataset.connect('sqlite:///nobel_prize:')
 
