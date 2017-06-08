fdea17fc1078fc5744bc8fd79ac551b1d850e323
Bill Westfall <billwestfall@yahoo.com>
2017-06-07 21:34:21 -0700

    Create scrape06.py

---
+++ dataviz/chapt05/scrape06.py
@@ -0,0 +1,15 @@
+import requests
+
+OECD_ROOT_URL = 'http://stats.oecd.org/sdmx-json/data'
+
+def make_OECD_request(dsname, dimensions, params=None, root_dir=OECD_ROOT_URL):
+
+    if not params:
+        params = {}
+
+    dim_args = ['+' .join(d) for d in dimensions]
+    dim_str = '.' .join(dim_args)
+
+    url = root_dir + '/' + dsname + '/' + dim_str + '/all'
+    print('Requesting URL: ' + url)
+    return requests.get(url, params=params)
