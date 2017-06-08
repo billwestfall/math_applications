dc832de460deef43296a286c4e83b0390af3bc34
Bill Westfall <billwestfall@yahoo.com>
2017-06-08 10:51:23 -0700

    Create scrape07.py

--- dataviz/chapt05/scrape07.py
+++ dataviz/chapt05/scrape07.py
@@ -1,27 +1,28 @@
 import requests
 
-REST_EU_ROOT_URL = 'http://restcountries.eu/rest/v2'
+REST_EU_ROOT_URL = 'http://restcountries.eu/rest/v2/all'
+url = 'http://restcountries.eu/rest/v2/all'
 
-def REST_country_request(field='all', name=None, params=None):
+#def REST_country_request(field='all', name=None, params=None):
     
-    headers={'User-Agent': 'Mozilla/5.0'}
+#    headers={'User-Agent': 'Mozilla/5.0'}
 
-    if not params:
-        params = {}
+#    if not params:
+#        params = {}
 
 #    if field == 'all':
-        return requests.get(REST_EU_ROOT_URL + '/all')
+#        return requests.get(REST_EU_ROOT_URL + '/all')
 
-    url = '%s/%s/%s'%(REST_EU_ROOT_URL, field, name)
-    print('Requesting URL: ' + url)
-    response = requests.get(url, params=params, headers=headers)
+#    url = '%s/%s/%s'%(REST_EU_ROOT_URL, field, name)
+#    print('Requesting URL: ' + url)
+    response = requests.get(url)
 
 #    if not response.status_code == 200:
 #        raise Exception('Request failed with status code ' + str(response.status_code))
 
     return response
-    response = REST_country_request('currency', 'usd')
-    response.json()
-    d = response.json()
-    print(d)
-    print(response.status_code)
+#    response = REST_country_request('currency', 'usd')
+#    response.json()
+#    d = response.json()
+#    print(d)
+#    print(response.status_code)
