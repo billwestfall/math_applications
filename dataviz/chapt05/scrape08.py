da126194f4f6584c0bf3e90acd61c798e180a268
Bill Westfall <billwestfall@yahoo.com>
2017-06-08 10:54:09 -0700

    Create scrape08.py

--- dataviz/chapt05/scrape08.py
+++ dataviz/chapt05/scrape08.py
@@ -1,8 +1,4 @@
 import requests
 
-REST_EU_ROOT_URL = 'http://restcountries.eu/rest/v2/all'
-url = 'http://restcountries.eu/rest/v2/all'
-
-response = requests.get(url)
-
-return response
+response = requests.get('http://restcountries.eu/rest/v2/all')
+print(response)
