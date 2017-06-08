a18293c25c4471246be072d68def27da1ee6ba86
Bill Westfall <billwestfall@yahoo.com>
2017-06-08 11:20:51 -0700

    Create scrape011.py

--- dataviz/chapt05/scrape011.py
+++ dataviz/chapt05/scrape011.py
@@ -2,7 +2,9 @@
 import requests
 from pymongo import MongoClient
 
-def get_mongo_database(db_name, host='localhost', port=27017, username=None, password=None)
+def get_mongo_database(db_name, host='localhost', port=27017, username=None, password=None):
+    mongo_uri = 'mongodb://localhost:27017'
+    conn = MongoClient(mongo_uri)
 
 REST_EU_ROOT_URL = 'http://restcountries.eu/rest/v2/all'
 url = 'http://restcountries.eu/rest/v2/all'
