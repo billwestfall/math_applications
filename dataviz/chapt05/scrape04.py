0eb1da2ffdb926b633cd14af75d9644d1059c3bf
Bill Westfall <billwestfall@yahoo.com>
2017-06-07 20:46:01 -0700

    Create scrape04.py

--- dataviz/chapt05/scrape04.py
+++ dataviz/chapt05/scrape04.py
@@ -1,5 +1,5 @@
 import requests
 
-response = requests.get("https://cdph.data.ca.gov/api/views/6tej-5zx7/rows.json?accessType=DOWNLOAD")
+response = requests.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=<APIKEY>&format=json")
 
 print(response.status_code)
