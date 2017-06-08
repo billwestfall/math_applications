c389bbc49f59fe4452c53ac8011f4f377fe1b53b
Bill Westfall <billwestfall@yahoo.com>
2017-06-07 21:07:00 -0700

    Create scrape05.py

--- dataviz/chapt05/scrape05.py
+++ dataviz/chapt05/scrape05.py
@@ -3,4 +3,5 @@ import requests
 response = requests.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=<APIKEY>&format=json")
 
 data = response.json()
-print(data.keys())
+d = data.keys()
+print(d)
