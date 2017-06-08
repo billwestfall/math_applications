3a97b150338005ace9786f43376d03982446c6a6
Bill Westfall <billwestfall@yahoo.com>
2017-06-08 14:32:24 -0700

    Create twitter03.py

--- dataviz/chapt05/twitter03.py
+++ dataviz/chapt05/twitter03.py
@@ -2,29 +2,28 @@
 
 import tweepy
 
-consumer_key = ''
-consumer_secret = ''
-
-access_token_key = ''
-access_token_secret = ''
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 auth1 = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth1.set_access_token(access_token_key, access_token_secret)
+auth1.set_access_token(access_token, access_token_secret)
 
 class StreamListener(tweepy.StreamListener):
     def on_status(self, tweet):
-        print 'Ran on_status'
+        print('Ran on_status')
 
     def on_error(self, status_code):
-        print 'Error: ' + repr(status_code)
+        print('Error: ' + repr(status_code))
         return False
 
     def on_data(self, data):
-        print 'Ok, this is actually running'
+        print('Ok, this is actually running')
 
 
 l = StreamListener()
 streamer = tweepy.Stream(auth=auth1, listener=l)
-#setTerms = ['hello', 'goodbye', 'goodnight', 'good morning']
-setTerms = ['twitter']
+setTerms = ['Hype Machine']
+#setTerms = ['twitter']
 streamer.filter(track = setTerms)
