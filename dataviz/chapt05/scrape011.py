e66fb5dbbdec17a9dd3f6de75650ab6269cd85aa
Bill Westfall <billwestfall@yahoo.com>
2017-06-08 11:31:31 -0700

    Create scrape011.py

--- dataviz/chapt05/scrape011.py
+++ dataviz/chapt05/scrape011.py
@@ -5,6 +5,10 @@ from pymongo import MongoClient
 def get_mongo_database(db_name, host='localhost', port=27017, username=None, password=None):
     mongo_uri = 'mongodb://localhost:27017'
     conn = MongoClient(mongo_uri)
+    
+def initialize(self):
+        client = pymongo.MongoClient(self.URI)
+        self.DATABASE = client['nobel_prize']
 
 REST_EU_ROOT_URL = 'http://restcountries.eu/rest/v2/all'
 url = 'http://restcountries.eu/rest/v2/all'
@@ -13,5 +17,5 @@ col = db_nobel['country_data']
 
 response = requests.get(url)
 
-client = MongoClient()
 col.insert(country_data)
+
