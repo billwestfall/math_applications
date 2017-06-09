7d34a3b52247129764ce5c6fc656868ffff20883
Bill Westfall <billwestfall@yahoo.com>
2017-06-09 14:48:39 -0700

    Create nobel01.py

--- dataviz/chapt05/nobel01.py
+++ dataviz/chapt05/nobel01.py
@@ -4,7 +4,7 @@ import requests
 
 def get_Nobel_soup():
     response = requests.get('https://en.wikipedia.org/wiki/List_of_Nobel_laureates')
-    returns BeautifulSoup4(response.content, "lxml")
+    returns BeautifulSoup(response, "html.parser")
 
 soup = get_Nobel_soup
 print(soup)
