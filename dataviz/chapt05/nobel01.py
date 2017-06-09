48cb47059dc64e6bf86efba4ad9ab875438718b8
Bill Westfall <billwestfall@yahoo.com>
2017-06-09 14:51:19 -0700

    Update nobel01.py

--- dataviz/chapt05/nobel01.py
+++ dataviz/chapt05/nobel01.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-    response = requests.get('https://en.wikipedia.org/wiki/List_of_Nobel_laureates')
-    soup = returns BeautifulSoup(response, "html.parser")
+response = requests.get('https://en.wikipedia.org/wiki/List_of_Nobel_laureates')
+soup = returns BeautifulSoup(response, "html.parser")
     
 print(soup.prettify())
