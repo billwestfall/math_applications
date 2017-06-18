0c6a07fe49617429c682e64eedcf788fcc3a0232
Bill Westfall <billwestfall@yahoo.com>
2017-06-18 12:15:53 -0700

    Update boolean02.py

--- miscellaneous/rosetta/boolean02.py
+++ miscellaneous/rosetta/boolean02.py
@@ -1,7 +1,13 @@
 checker = None
 test = "test"
 
-print("Boolean and test: ")
+print("Boolean true test: ")
 if test == test:
   checker = True
+  print(checker)
+print("Boolean false test: ")
+if test == 0:
+  checker = True
+  else 
+  checker = False
   print(checker)
