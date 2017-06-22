358458e0b7e7aefb5573d0ccf9de85eda41e8963
Bill Westfall <billwestfall@yahoo.com>
2017-06-22 15:47:22 -0700

    Create hype.py

---
+++ dataviz/chapt06/nobel_winners/nobel_winners/spiders/hype.py
@@ -0,0 +1,14 @@
+# run as scrapy runspider -t json -o hype.json hype.py
+
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://hypem.com/spy/RU']
+
+    def parse(self, response):
+        for title in response.css('h3.track_name'):
+            yield {'artist': title.css('a ::text').extract_first()}
+
+        for next_page in response.css('div.prev-post > a'):
+yield response.follow(next_page, self.parse)
