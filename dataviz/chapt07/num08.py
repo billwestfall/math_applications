4ce239c9c927025835cebb70494a65caf6f591aa
Bill Westfall <billwestfall@yahoo.com>
2017-06-23 13:07:06 -0700

    Create num08.py

---
+++ dataviz/chapt07/num08.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def print_array_details(a):
+    print('Dimensions: %d, shape: %s, dtype: %s' %(a.ndim, a.shape, a.dtype))
+
+a = np.array([1, 2, 3, 4, 5, 6])
+print("element 2 in array")
+print(a[2])
+print("elements 3 to 5 noninclusive")
+print(a[3:5])
+print("insert zeros")
+print(a[:4:2] = 0)
+print("reversed")
+print(a[::-1])
