fbc9755c050d42c83786905a1d649c8700ed504b
Bill Westfall <billwestfall@yahoo.com>
2017-06-23 12:17:17 -0700

    Update num02.py

--- dataviz/chapt07/num02.py
+++ dataviz/chapt07/num02.py
@@ -5,6 +5,6 @@ a = np.array([1, 2, 3, 4, 5, 6, 7, 8])
 print(a)
 
 def print_array_details(a):
-    print(Dimensions: %d, shape: %s, dtype: %s' %(a.ndim, a,shape, a.dtype))
+    print('Dimensions: %d, shape: %s, dtype: %s'%(a.ndim, a,shape, a.dtype))
 
 print_array_details(a)
