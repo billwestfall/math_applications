c736f4edc31c3c8848bc7ba176ffef96c3b8a730
Bill Westfall <billwestfall@yahoo.com>
2017-06-23 12:24:31 -0700

    Create num03.py

---
+++ dataviz/chapt07/num03.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+print(a)
+
+def print_array_details(a):
+    print('Dimensions: %d, shape: %s, dtype: %s' %(a.ndim, a.shape, a.dtype))
+
+print_array_details(a)
+
+a = a.reshape([2, 4])
+print("as a two dimensional array:")
+print(a)
+print_array_details(a)
+a = a.reshape([2, 2, 2])
+print("as a three dimensional array:")
+print(a)
+print_array_details(a)
