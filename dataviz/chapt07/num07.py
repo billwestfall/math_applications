16f4acdc09bed3560d2ee30ba15789bb12bd948b
Bill Westfall <billwestfall@yahoo.com>
2017-06-23 12:57:33 -0700

    Update num07.py

--- dataviz/chapt07/num07.py
+++ dataviz/chapt07/num07.py
@@ -3,8 +3,8 @@ import numpy as np
 def print_array_details(a):
     print('Dimensions: %d, shape: %s, dtype: %s' %(a.ndim, a.shape, a.dtype))
 
-line_array = np.linespace(2, 10, 5)
-print("a linespace array")
+line_array = np.linspace(2, 10, 5)
+print("a linspace array")
 print(line_array)
 
 arrange_array = np.arrange(2, 10, 2)
