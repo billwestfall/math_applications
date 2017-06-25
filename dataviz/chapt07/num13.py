87a9eadf56373ca3984d1efe33c5ac347df8b5dd
Bill Westfall <billwestfall@yahoo.com>
2017-06-25 14:09:35 -0700

    Create num13.py

---
+++ dataviz/chapt07/num13.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+a = np.arange(6)
+print("The array")
+csum = np.csum(a)
+print(csum)
