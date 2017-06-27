3cf8d20a16a33f98f85b764eec41fc75d33108fc
Bill Westfall <billwestfall@yahoo.com>
2017-06-27 14:03:44 -0700

    Update last.py

--- dataviz/chapt08/last.py
+++ dataviz/chapt08/last.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_json('last.json')
-print(df)
+#print(df)
 print(df.columns)
-print(df.index)
-df = df.set_index('weeklytrackchart.track')
-print(df)
-print(df.loc('Psychorama'))
-df = df.reset_index()
-print(df)
+#print(df.index)
+#df = df.set_index('weeklytrackchart.track')
+#print(df)
+#print(df.loc('Psychorama'))
+#df = df.reset_index()
+#print(df)
