a289e10373666c71c1c21256c11cdc106be3171b
Bill Westfall <billwestfall@yahoo.com>
2017-06-27 13:13:21 -0700

    Update dataframe01.py

--- dataviz/chapt08/dataframe01.py
+++ dataviz/chapt08/dataframe01.py
@@ -4,7 +4,7 @@ import numpy as np
 
 todays_date = datetime.datetime.now().date()
 index = pd.date_range(todays_date-datetime.timedelta(10), periods=10, freq='D')
-
+data = np.array([np.arange(10)]*3).T
 columns = ['A','B', 'C']
 
 df = pd.DataFrame(data, index=index, columns=columns)
