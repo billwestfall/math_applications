200687ba6dca97ac4136110142d1fb8c77d31316
Bill Westfall <billwestfall@yahoo.com>
2017-06-29 14:54:15 -0700

    Update clean001.py

--- dataviz/chapt09/clean001.py
+++ dataviz/chapt09/clean001.py
@@ -2,3 +2,15 @@ import pandas as pd
 
 df = pd.read_json(open(data/nobel_winners_dirty.json))
 
+def mongo_to_dataframe(db_name, collection, query={}), host='localhost', port=27017, username=none, password=none, no_id=true):
+    db = get_mongo_database(db_name, host, port, username, password)
+    cursor = db[collection].find(query)
+    df = pd.dataframe(list(cursor))
+
+    if no_id:
+        del df['_id']
+
+    return df
+
+df = mongo_to_dataframe('nobel_prize', 'winners')
+print(df)
