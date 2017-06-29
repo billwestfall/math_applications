469c6c0be5c44365fd0cc35004754cb21f477cb8
Bill Westfall <billwestfall@yahoo.com>
2017-06-29 15:08:56 -0700

    Update clean001.py

--- dataviz/chapt09/clean001.py
+++ dataviz/chapt09/clean001.py
@@ -3,10 +3,10 @@ from pymongo import MongoClient
 
 df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
-def get_mongo_database(db_name, collection, query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+def get_mongo_database(db_name, collection, query={}, host='localhost', port="27017", username="", password="", no_id="true"):
     conn = MongoClient(host, port)
 
-def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port="27017", username="", password="", no_id="true"):
     db = get_mongo_database(db_name, host, port, username, password)
     cursor = db[collection].find(query)
     df = pd.dataframe(list(cursor))
