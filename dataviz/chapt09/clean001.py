53677113506788f3a01561e7374d73f1effae8ba
Bill Westfall <billwestfall@yahoo.com>
2017-06-29 15:25:32 -0700

    Update clean001.py

--- dataviz/chapt09/clean001.py
+++ dataviz/chapt09/clean001.py
@@ -3,18 +3,17 @@ from pymongo import MongoClient
 
 df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
-def get_mongo_database(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
-    conn = MongoClient()
-
-def mongo_to_dataframe(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
-    db = get_mongo_database(db_name, collection)
-    cursor = db[collection].find(query)
-    df = pd.dataframe(list(cursor))
-
-    if no_id:
-        del df['_id']
-
-    return df
-
-df = mongo_to_dataframe('nobel_prize', 'winners')
 print(df)
+
+#def get_mongo_database(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+#    conn = MongoClient()
+
+#def mongo_to_dataframe(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+#    db = get_mongo_database(db_name, collection)
+#    cursor = db[collection].find(query)
+#    df = pd.dataframe(list(cursor))
+
+#    if no_id:
+#        del df['_id']
+#
+#    return df
