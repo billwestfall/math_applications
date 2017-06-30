0351b7547a21ea3a1927c3d568c67823f672d492
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 14:08:48 -0700

    Update clean001.py

--- dataviz/chapt09/clean001.py
+++ dataviz/chapt09/clean001.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
 print(df)
+print(df.info())
 
 #def get_mongo_database(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
 #    conn = MongoClient()
@@ -18,22 +19,22 @@ print(df)
 #
 #    return df
 
-import csv
-import json
-import pandas as pd
-import sys, getopt, pprint
-from pymongo import MongoClient
+#import csv
+#import json
+#import pandas as pd
+#import sys, getopt, pprint
+#from pymongo import MongoClient
 #CSV to JSON Conversion
-csvfile = open('C://test//final-current.csv', 'r')
-reader = csv.DictReader( csvfile )
-mongo_client=MongoClient() 
-db=mongo_client.october_mug_talk
-db.segment.drop()
-header= [ "S No", "Instrument Name", "Buy Price", "Buy Quantity", "Sell Price", "Sell Quantity", "Last Traded Price", "Total Traded Quantity", "Average Traded Price", "Open Price", "High Price", "Low Price", "Close Price", "V" ,"Time"]
+#csvfile = open('C://test//final-current.csv', 'r')
+#reader = csv.DictReader( csvfile )
+#mongo_client=MongoClient() 
+#db=mongo_client.october_mug_talk
+#db.segment.drop()
+#header= [ "S No", "Instrument Name", "Buy Price", "Buy Quantity", "Sell Price", "Sell Quantity", "Last Traded Price", "Total Traded Quantity", "Average Traded Price", "Open Price", "High Price", "Low Price", "Close Price", "V" ,"Time"]
 
-for each in reader:
-    row={}
-    for field in header:
-        row[field]=each[field]
+#for each in reader:
+#    row={}
+#    for field in header:
+#        row[field]=each[field]
 
-    db.segment.insert(row)
+#    db.segment.insert(row)
