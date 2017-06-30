bf68be3c9df62065db0fb83ab77e81b835589f73
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 14:42:45 -0700

    Create clean005.py

---
+++ dataviz/chapt09/clean005.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from pymongo import MongoClient
+
+df = pd.read_json(open("data/nobel_winners_dirty.json"))
+
+print("Describe born in column")
+print(df.born_in.describe())
+print("Setting born in to unicode")
+set(df.born_in.apply(type))
+print(df.born_in.describe())
