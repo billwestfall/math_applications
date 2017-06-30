6d7c60835581bb9047aaf3c04ee682b7996684e0
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 14:21:34 -0700

    Update clean002.py

--- dataviz/chapt09/clean002.py
+++ dataviz/chapt09/clean002.py
@@ -4,3 +4,7 @@ from pymongo import MongoClient
 df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
 print(df.columns)
+print(df.set_index('name'))
+print(df.head(2))
+print(df.reset_index(inplace=True))
+print(df.head(2))
