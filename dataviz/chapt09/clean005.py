88a94fc9fdaa10e393934cc90342172c5c49cf27
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 14:46:51 -0700

    Update clean005.py

--- dataviz/chapt09/clean005.py
+++ dataviz/chapt09/clean005.py
@@ -3,8 +3,13 @@ from pymongo import MongoClient
 
 df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
+bi_col = df['born_in']
 print("Describe born in column")
 print(df.born_in.describe())
 print("Setting born in to unicode")
 set(df.born_in.apply(type))
 print(set(df.born_in.apply(type)))
+bi_col.replace('', np.nan, inplace=True)
+print("Replace missing values with NaN")
+print(bi_col)
+print(bi_col.count())
