517b963916df4bb37034e51eb6f63f671b9ad264
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 15:23:09 -0700

    Update clean007.py

--- dataviz/chapt09/clean007.py
+++ dataviz/chapt09/clean007.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 df2 = pd.DataFrame({'name':['zak', 'alice', 'bob', 'mike', 'bob', 'bob'], 'score':[4, 3, 5, 2, 3, 7]})
 
-frame_sort = df2.sort_values(['name', 'score'], ascending=[1.0])
+frame_sort = df2.sort_values(['name', 'score'], ascending=[1,0])
 
 print("sort on dataframe name and score values")
 print(frame_sort)
