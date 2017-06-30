862889034d964b7fc9ebc76ea10dc307d3d77068
Bill Westfall <billwestfall@yahoo.com>
2017-06-30 14:22:42 -0700

    Update clean002.py

--- dataviz/chapt09/clean002.py
+++ dataviz/chapt09/clean002.py
@@ -5,6 +5,8 @@ df = pd.read_json(open("data/nobel_winners_dirty.json"))
 
 print(df.columns)
 print(df.set_index('name'))
+print("Set index on name")
 print(df.head(2))
 print(df.reset_index(inplace=True))
+print("Reset index")
 print(df.head(2))
