1521046882ec0ed6ab960455ff42258a204f97a9
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 12:58:52 -0700

    Create plot008.py

---
+++ dataviz/chapt010/plot008.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+labels = ["Physics", "Chemistry", "Literature", "Peace"]
+foo_data = [3, 6, 10, 4]
+bar_data = [8, 3, 6, 1]
+
+fig, ax = plt.subplots(figsize=(8, 4))
+bar_width = 0.4
+xlocs = np.arange(len(foo_data))
+ax.bar(xlocs-bar_width, foo_data, bar_width, color='#fde0bc', label='Fooland')
+ax.bar(xlocs, foo_data, bar_data, bar_width, color='peru', label='Barland')
+ax.set_yticks(range(12))
+ax.set_xticks(ticks=range(len(foo_data)))
+ax.set_xticklabels(labels)
+ax.yaxis.grid(True)
+ax.legend(loc='best')
+ax.set_ylabel('Number of prizes')
+fig.suptitle('Prizes by country')
+fig.tight_layout(pad=2)
+fig.savefig('mpl_barchart_multi.png', dpi=200)
