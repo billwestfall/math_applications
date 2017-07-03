5c47206cc6db925e707f8429d7378dfa35cae282
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 11:50:48 -0700

    Update plot002.py

--- dataviz/chapt010/plot002.py
+++ dataviz/chapt010/plot002.py
@@ -7,7 +7,7 @@ x = pd.period_range(pd.datetime.now(), periods=200, freq='d')
 x = x.to_timestamp().to_pydatetime()
 y = np.random.randn(200, 3).cumsum(0)
 mpl.rcParams['lines.linewidth'] = 2
-mpl.reParams['lines.color'] = 'r'
+mpl.rcParams['lines.color'] = 'r'
 
 plt.plot(x, y)
 plt.show()
