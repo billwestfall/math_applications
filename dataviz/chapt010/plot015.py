94c76a122a94dd5e209601f549d4352f14c80e4a
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 13:39:53 -0700

    Update plot015.py

--- dataviz/chapt010/plot015.py
+++ dataviz/chapt010/plot015.py
@@ -4,6 +4,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+x = np.array(range(num_points))
+y = np.random.randn(num_points) * 10 + x*gradient
 data = pd.DataFrame({'dummy x':x, 'dummy y' :y})
 sns.lmplot('dummy x', 'dummy y', data, size=4, aspect=2)
 
