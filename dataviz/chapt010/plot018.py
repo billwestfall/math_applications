8ca74f3485745757d3527ae0da530ea970f3fff7
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 14:36:58 -0700

    Create plot018.py

---
+++ dataviz/chapt010/plot018.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+tips = sns.load_dataset('tips')
+print(tips)
+
+g = sns.FacetGrid(tips, col="sex")
+g.map(plt.scatter, "total_bill", "tip")
