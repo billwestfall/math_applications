96d8917b8e51304579c4c62ee8ce782c56fe2a80
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 11:50:09 -0700

    Create plot002.py

---
+++ dataviz/chapt010/plot002.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+x = pd.period_range(pd.datetime.now(), periods=200, freq='d')
+x = x.to_timestamp().to_pydatetime()
+y = np.random.randn(200, 3).cumsum(0)
+mpl.rcParams['lines.linewidth'] = 2
+mpl.reParams['lines.color'] = 'r'
+
+plt.plot(x, y)
+plt.show()
