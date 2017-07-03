9d0da7f154c160afcbd86a65be501ceaeda11f96
Bill Westfall <billwestfall@yahoo.com>
2017-07-03 12:21:19 -0700

    Update plot005.py

--- dataviz/chapt010/plot005.py
+++ dataviz/chapt010/plot005.py
@@ -3,6 +3,10 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+x = pd.period_range(pd.datetime.now(), periods=200, freq='d')
+x = x.to_timestamp().to_pydatetime()
+y = np.random.randn(200, 3).cumsum(0)
+
 fig = plt.figure(figsize=(8,4))
 ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 ax.set_title('Main axes with child insert axes')
