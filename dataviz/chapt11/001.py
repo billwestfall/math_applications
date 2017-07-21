3e1bb605bfbd001b19e887531fff78a8dd76cb6b
Bill Westfall <billwestfall@yahoo.com>
2017-07-21 11:17:50 -0700

    Update 001.py

--- dataviz/chapt11/001.py
+++ dataviz/chapt11/001.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import json
 import seaborn as sb
 
+def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port=27017):
+  db = 
+
 plt.rcParams['figure.figsize'] = 8, 4
 
 df = mongo_to_dataframe('nobel_prize', 'winners_clean')
