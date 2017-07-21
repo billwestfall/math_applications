b3e54ffee3c545b03f29335c1635d55c51f52611
Bill Westfall <billwestfall@yahoo.com>
2017-07-21 11:19:04 -0700

    Update 001.py

--- dataviz/chapt11/001.py
+++ dataviz/chapt11/001.py
@@ -4,8 +4,18 @@ import matplotlib.pyplot as plt
 import json
 import seaborn as sb
 
-def mongo_to_dataframe(db_name, collection, query={}, host='localhost', port=27017):
-  db = 
+def get_mongo_database(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+    conn = MongoClient()
+
+def mongo_to_dataframe(db_name="nobel_prize", collection="winners", query={}, host='localhost', port=27017, username="", password="", no_id="true"):
+    db = get_mongo_database(db_name, collection)
+    cursor = db[collection].find(query)
+    df = pd.dataframe(list(cursor))
+
+    if no_id:
+        del df['_id']
+
+    return df
 
 plt.rcParams['figure.figsize'] = 8, 4
 
