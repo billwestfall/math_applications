792a0f180e0c748a7997ba7095443f446f6fdab6
Bill Westfall <billwestfall@yahoo.com>
2020-05-30 17:04:53 -0700

    Create 002_jokes.py

---
+++ miscellaneous/no_starch/games/002_jokes.py
@@ -0,0 +1,8 @@
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+print('What do dentists call an astronaut\'s cavity?')
+input()
+print('A black hole!')
+print()
