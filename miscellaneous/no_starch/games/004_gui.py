81bac3b83394b2fd7dfa227b0c0abf1057031fb8
Bill Westfall <billwestfall@yahoo.com>
2020-06-03 16:33:51 -0700

    Update 004_gui.py

--- miscellaneous/no_starch/games/004_gui.py
+++ miscellaneous/no_starch/games/004_gui.py
@@ -8,7 +8,7 @@ colours = ['Red','Blue','Green','Pink','Black',
 score = 0
 
 # the game time left, initially 30 seconds. 
-timeleft = 30
+timeleft = 60
 
 # function that will start the game. 
 def startGame(event): 
