8d701951e35fac70f469a99bf5f53d49924a0b2e
Bill Westfall <billwestfall@yahoo.com>
2020-06-03 18:16:16 -0700

    Update 005_gui_pic.py

--- miscellaneous/no_starch/games/005_gui_pic.py
+++ miscellaneous/no_starch/games/005_gui_pic.py
@@ -1,10 +1,10 @@
 from tkinter import *  
 from PIL import ImageTk,Image
 
-    root = Tk()  
-    canvas = Canvas(root, width = 300, height = 300)  
-    canvas.pack()  
-    img = ImageTk.PhotoImage(Image.open("ball.png"))  
-    canvas.create_image(20, 20, anchor=NW, image=img) 
-    root.mainloop() 
+root = Tk()  
+canvas = Canvas(root, width = 300, height = 300)  
+canvas.pack()  
+img = ImageTk.PhotoImage(Image.open("ball.png"))  
+canvas.create_image(20, 20, anchor=NW, image=img) 
+root.mainloop() 
 
