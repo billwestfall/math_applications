7048b8daa008932fa47338fab3fde67b198e28a9
Bill Westfall <billwestfall@yahoo.com>
2020-08-31 21:18:55 -0700

    Update test.py

--- miscellaneous/test/test.py
+++ miscellaneous/test/test.py
@@ -1,8 +1,7 @@
 import random
-from random import choice
 
 cardList = ["Ace \[ClubSuit]", "9 \[ClubSuit]", "10 \[ClubSuit]", "Jack \[ClubSuit]", "Queen \[ClubSuit]", "King \[ClubSuit]", "Ace \[SpadeSuit]", "9 \[SpadeSuit]", "10 \[SpadeSuit]", "Jack \[SpadeSuit]", "Queen \[SpadeSuit]", "King \[SpadeSuit]", "Ace \[DiamondSuit]", "9 \[DiamondSuit]", "10 \[DiamondSuit]", "Jack \[DiamondSuit]", "Queen \[DiamondSuit]", "King \[DiamondSuit]", "Ace \[HeartSuit]", "9 \[HeartSuit]", "10 \[HeartSuit]", "Jack \[HeartSuit]", "Queen \[HeartSuit]", "King \[HeartSuit]"]
 
 mylist = ["apple", "banana", "cherry"]
 
-print(random.choices(mylist, weights = [10, 1, 1], k = 14))
+print(random.sample(mylist, weights = [10, 1, 1], k = 14))
