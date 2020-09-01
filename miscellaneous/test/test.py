c1a0bc39cc1d325f45a854f234df704d616282a0
Bill Westfall <billwestfall@yahoo.com>
2020-08-31 21:19:57 -0700

    Update test.py

--- miscellaneous/test/test.py
+++ miscellaneous/test/test.py
@@ -2,6 +2,4 @@ import random
 
 cardList = ["Ace \[ClubSuit]", "9 \[ClubSuit]", "10 \[ClubSuit]", "Jack \[ClubSuit]", "Queen \[ClubSuit]", "King \[ClubSuit]", "Ace \[SpadeSuit]", "9 \[SpadeSuit]", "10 \[SpadeSuit]", "Jack \[SpadeSuit]", "Queen \[SpadeSuit]", "King \[SpadeSuit]", "Ace \[DiamondSuit]", "9 \[DiamondSuit]", "10 \[DiamondSuit]", "Jack \[DiamondSuit]", "Queen \[DiamondSuit]", "King \[DiamondSuit]", "Ace \[HeartSuit]", "9 \[HeartSuit]", "10 \[HeartSuit]", "Jack \[HeartSuit]", "Queen \[HeartSuit]", "King \[HeartSuit]"]
 
-mylist = ["apple", "banana", "cherry"]
-
-print(random.sample(mylist, k = 14))
+print(random.sample(cardList, k = 5))
