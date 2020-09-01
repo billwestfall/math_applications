0a05b3a8c3d0259a2df8fbb559fcbccb23a7dc4a
Bill Westfall <billwestfall@yahoo.com>
2020-08-31 21:12:51 -0700

    Update test.py

--- miscellaneous/test/test.py
+++ miscellaneous/test/test.py
@@ -2,5 +2,4 @@ import random
 
 cardList = ["Ace \[ClubSuit]", "9 \[ClubSuit]", "10 \[ClubSuit]", "Jack \[ClubSuit]", "Queen \[ClubSuit]", "King \[ClubSuit]", "Ace \[SpadeSuit]", "9 \[SpadeSuit]", "10 \[SpadeSuit]", "Jack \[SpadeSuit]", "Queen \[SpadeSuit]", "King \[SpadeSuit]", "Ace \[DiamondSuit]", "9 \[DiamondSuit]", "10 \[DiamondSuit]", "Jack \[DiamondSuit]", "Queen \[DiamondSuit]", "King \[DiamondSuit]", "Ace \[HeartSuit]", "9 \[HeartSuit]", "10 \[HeartSuit]", "Jack \[HeartSuit]", "Queen \[HeartSuit]", "King \[HeartSuit]"]
 
-sampling = random.choices(cardList, k=5)
-print("random item from list is: ", sampling)
+print(random.choices(cardList, k = 5))
