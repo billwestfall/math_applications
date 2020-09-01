0937af966b8655a0e8fdc55ae143a957613df618
Bill Westfall <billwestfall@yahoo.com>
2020-08-31 21:16:01 -0700

    Update test.py

--- miscellaneous/test/test.py
+++ miscellaneous/test/test.py
@@ -4,4 +4,4 @@ cardList = ["Ace \[ClubSuit]", "9 \[ClubSuit]", "10 \[ClubSuit]", "Jack \[ClubSu
 
 mylist = ["apple", "banana", "cherry"]
 
-print(random.choices(mylist, weights = [10, 1, 1], k = 14)) 
+print(random.choices(mylist, weights = [10, 1, 1], k = 14))
