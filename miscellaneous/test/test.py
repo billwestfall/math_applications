a81fa51def2104f5cb2039e0a603f79fd68aa2b1
Bill Westfall <billwestfall@yahoo.com>
2020-08-31 21:01:38 -0700

    Update test.py

--- miscellaneous/test/test.py
+++ miscellaneous/test/test.py
@@ -1,4 +1,5 @@
 import random
 
-numberList = [111,222,333,444,555]
-print("random item from list is: ", random.choice(numberList))
+cardList = ["Ace \[ClubSuit]", "9 \[ClubSuit]", "10 \[ClubSuit]", "Jack \[ClubSuit]", "Queen \[ClubSuit]", "King \[ClubSuit]", "Ace \[SpadeSuit]", "9 \[SpadeSuit]", "10 \[SpadeSuit]", "Jack \[SpadeSuit]", "Queen \[SpadeSuit]", "King \[SpadeSuit]", "Ace \[DiamondSuit]", "9 \[DiamondSuit]", "10 \[DiamondSuit]", "Jack \[DiamondSuit]", "Queen \[DiamondSuit]", "King \[DiamondSuit]", "Ace \[HeartSuit]", "9 \[HeartSuit]", "10 \[HeartSuit]", "Jack \[HeartSuit]", "Queen \[HeartSuit]", "King \[HeartSuit]"]
+
+print("random item from list is: ", random.choice(cardList))
