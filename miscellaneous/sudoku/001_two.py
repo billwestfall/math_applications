ce538e60c056dd2ab9e62520fcbc1a4739123e87
Bill Westfall <billwestfall@yahoo.com>
2022-04-10 11:57:52 -0700

    Update 001_two.py

--- miscellaneous/sudoku/001_two.py
+++ miscellaneous/sudoku/001_two.py
@@ -1,5 +1,6 @@
 import numpy as np
- 
+
+a1 = [3 3]
 arr = np.random.randint(1, 3, size = (2, 2))
 arr_sum = np.sum(arr, axis=1)
 arr_sumb = np.sum(arr, axis=0)
@@ -7,7 +8,7 @@ arr_sumb = np.sum(arr, axis=0)
 print('-----Generated Random Array----')
 print(arr)
 
-if arr_sum == [3 3]:
+if arr_sumarray_equal(a1):
  print(arr_sum)
 else:
  print("Not sudoku")
