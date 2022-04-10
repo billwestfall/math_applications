95f67b973f635f5d0fb9cb200a7a3a8ca7f1e7ee
Bill Westfall <billwestfall@yahoo.com>
2022-04-10 11:05:20 -0700

    Update 001_two.py

--- miscellaneous/sudoku/001_two.py
+++ miscellaneous/sudoku/001_two.py
@@ -2,7 +2,9 @@ import numpy as np
  
 arr = np.random.randint(1, 3, size = (2, 2))
 arr_sum = np.sum(arr, axis=1)
+arr_sumb = np.sum(arr, axis=0)
  
 print('-----Generated Random Array----')
 print(arr)
 print(arr_sum)
+print(arr_sumb)
