72b7dc043314b947797e85c4f68b670d92515e49
Bill Westfall <billwestfall@yahoo.com>
2022-04-10 10:40:27 -0700

    Update 001_two.py

--- miscellaneous/sudoku/001_two.py
+++ miscellaneous/sudoku/001_two.py
@@ -1,11 +1,6 @@
-import random
-z = random.randint(1, 2)
-w = random.sample(random.randint(1, 2), 2)
-v = random.sample(random.randint(1, 2), 2)
-u = random.sample(random.randint(1, 2), 2)
-t = random.sample(random.randint(1, 2), 2)
-
-A = [[w], [v]]
-B = [A[0][1]]
-print(A)
-print(B)
+import numpy as np
+ 
+arr = np.random.randint(0, 5, size = 4)
+ 
+print('-----Generated Random Array----')
+print(arr)
