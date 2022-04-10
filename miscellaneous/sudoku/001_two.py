dc4bd9f1f3e7311a525df7d8018aa8cc8b98d7ec
Bill Westfall <billwestfall@yahoo.com>
2022-04-10 11:21:40 -0700

    Update 001_two.py

--- miscellaneous/sudoku/001_two.py
+++ miscellaneous/sudoku/001_two.py
@@ -6,5 +6,10 @@ arr_sumb = np.sum(arr, axis=0)
  
 print('-----Generated Random Array----')
 print(arr)
+
+if arr_sum == "[3 3]":
+ print(arr_sum)
+else:
+ print("Not sudoku")
 print(arr_sum)
 print(arr_sumb)
