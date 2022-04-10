c851001f5b2a43a69d93303bcb3dffb07a01ec2d
Bill Westfall <billwestfall@yahoo.com>
2022-04-10 10:33:37 -0700

    Update 001_two.py

--- miscellaneous/sudoku/001_two.py
+++ miscellaneous/sudoku/001_two.py
@@ -1,7 +1,11 @@
 import random
 z = random.randint(1, 2)
+w = random.sample(random.randint(1, 2), 2)
+v = random.sample(random.randint(1, 2), 2)
+u = random.sample(random.randint(1, 2), 2)
+t = random.sample(random.randint(1, 2), 2)
 
-A = [[z, z], [z, z]]
+A = [[w], [v]]
 B = [A[0][1]]
 print(A)
 print(B)
