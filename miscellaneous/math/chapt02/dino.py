f671990d78312d20f79b73b3aadf6fc3a9502257
Bill Westfall <billwestfall@yahoo.com>
2022-10-31 15:00:28 -0700

    Update dino.py

--- miscellaneous/math/chapt02/dino.py
+++ miscellaneous/math/chapt02/dino.py
@@ -1,5 +1,5 @@
 from vector_drawing import *
-     dino_vectors = [(6,4), (3,1), (1,2), (–1,5), (–2,5), (–3,4), (–4,4),
+dino_vectors = [(6,4), (3,1), (1,2), (–1,5), (–2,5), (–3,4), (–4,4),
      # insert 16 remaining vectors here
 ]
 draw(
