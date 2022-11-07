5d181c85a7a36f0224625dc87ebc8500166bcbb6
Bill Westfall <billwestfall@yahoo.com>
2022-11-07 13:58:48 -0800

    Update patient001.py

--- miscellaneous/test/patient001.py
+++ miscellaneous/test/patient001.py
@@ -1,6 +1,7 @@
 # Create a pivot table of the start (minimum) and end (maximum) timestamps associated with each case:
 case_starts_ends = events.pivot_table(index='patient', aggfunc={'datetime': ['min', 'max']}) 
-case_starts_ends = case_starts_ends.reset_index() case_starts_ends.columns = ['patient', 'caseend', 'casestart'] 
+case_starts_ends = case_starts_ends.reset_index() 
+case_starts_ends.columns = ['patient', 'caseend', 'casestart'] 
 # Merge with the main event log data so that for each row we have the start and end times.
 events = events.merge(case_starts_ends, on='patient') 
 # Calculate the relative time by subtracting the process start time from the event timestamp
