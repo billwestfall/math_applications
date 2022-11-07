f7c794150637b4c5da9e45b552eac4a44056ae54
Bill Westfall <billwestfall@yahoo.com>
2022-11-07 13:39:36 -0800

    Create process001.py

---
+++ miscellaneous/test/process001.py
@@ -0,0 +1,20 @@
+# data
+import pandas as pd
+from pm4py.objects.conversion.log import converter as log_converter
+from pm4py.objects.log.importer.xes import importer as xes_importer
+from pm4py.objects.log.util import dataframe_utils
+
+# process mining 
+from pm4py.algo.discovery.alpha import algorithm as alpha_miner
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
+from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
+
+# viz
+from pm4py.visualization.petrinet import visualizer as pn_visualizer
+from pm4py.visualization.process_tree import visualizer as pt_visualizer
+from pm4py.visualization.heuristics_net import visualizer as hn_visualizer
+from pm4py.visualization.dfg import visualizer as dfg_visualization
+
+# misc 
+from pm4py.objects.conversion.process_tree import converter as pt_converter
