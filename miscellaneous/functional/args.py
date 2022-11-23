fa6bc1f69569051d40d0808f0f2890d5c312fdef
Bill Westfall <billwestfall@yahoo.com>
2022-11-23 14:33:01 -0800

    Create args.py

---
+++ miscellaneous/functional/args.py
@@ -0,0 +1,9 @@
+import math
+
+def combine_2_and_3(func):
+  return func(2, 3}
+              
+print(combine_2_and_3(math.add))
+print(combine_2_and_3(math.subtract))
+print(combine_2_and_3(math.divide))
+print(combine_2_and_3(math.multiply))
