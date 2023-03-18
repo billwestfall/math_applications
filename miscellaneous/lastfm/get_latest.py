cbb693daf90ebe917d46b6c228552ab079d2903f
Bill Westfall <billwestfall@yahoo.com>
2023-03-18 09:45:34 -0700

    Create get_latest.py

---
+++ miscellaneous/lastfm/get_latest.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+headers = {
+    'user-agent': "cmd_api"
+}
+
+payload = {
+    'api_key': '<key>',
+    'method': 'user.getrecenttracks',
+    'format': 'json',
+    'user': '<user>',
+    'limit': '1'
+}
+
+r = requests.get('http://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+status = r.status_code
+y = (r.json())
+print(y["recenttracks"]["track"][0]["artist"]['#text'])
+print(y["recenttracks"]["track"][0]["name"])
