c7f2180cdf516e6d8c4e4053856413ea9cd64bd0
Bill Westfall <billwestfall@yahoo.com>
2023-05-03 09:27:50 -0700

    Update random_print.py

--- miscellaneous/math/chapt02/random_print.py
+++ miscellaneous/math/chapt02/random_print.py
@@ -1,3 +1,4 @@
 import random, string
-x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in random.randint(5, 16))
+random.randint(5, 16)
+x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in y)
 print(x)
