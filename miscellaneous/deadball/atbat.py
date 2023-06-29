d380f1dab334bf0bfbb949e32b6da8dba8af0b7e
Bill Westfall <billwestfall@yahoo.com>
2023-06-29 13:50:58 -0700

    Create atbat.py

---
+++ miscellaneous/deadball/atbat.py
@@ -0,0 +1,42 @@
+import random
+
+print("Enter first two digits of batter's batting average (no decimal):")
+
+a = input()
+b = int(a)
+
+print("Enter the pitcher's ERA (no decimal):")
+
+c = input()
+d = int(c)
+
+bat = random.randint(10, 100, 10)
+
+if b = range(0,100):
+    roll = random.randint(1, 20)
+
+elif b = range(101,200):
+    roll = random.randint(1, 12)
+
+elif b = range(201,300):
+    roll = random.randint(1, 8)
+
+elif b = range(301,350):
+    roll = random.randint(1, 4)
+
+elif b = range(351,400):
+    roll = random.randint(-1, -4)
+
+elif b = range(401,500):
+    roll = random.randint(-1, -8)
+
+elif b = range(501,600):
+    roll = random.randint(-1, -12)
+
+elif b = range(601,700):
+    roll = random.randint(-1, -20)
+
+else:
+    roll = -20
+
+    
