25c09be7c8e25367e986d309751d369c8ded7cda
Bill Westfall <billwestfall@yahoo.com>
2023-07-17 12:11:46 -0700

    Update 002.py

--- miscellaneous/math/002/002.py
+++ miscellaneous/math/002/002.py
@@ -7,6 +7,7 @@ def f(x):
   return x*(x - 2)*np.exp(3 - x)
   
 x = np.linspace(-0.5, 3.0)
-lines = plt.plot(x, f(x), x, x**2, x, 1 - x)
+#lines = plt.plot(x, f(x), x, x**2, x, 1 - x)
 
-lines.show()
+plt.plot(x, f(x), x, x**2, x, 1 - x)
+plt.show()
