8d765ec7836862703f71524321d9cf76a4c2bf32
Bill Westfall <billwestfall@yahoo.com>
2023-07-17 12:03:03 -0700

    Create 002.py

---
+++ miscellaneous/math/002/002.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure() # manually create a figure
+lines = plt.plot(x, y) # plot data
+
+x = np.linspace(-0.5, 3.0)
+lines = plt.plot(x, f(x), x, x**2, x, 1 - x)
+
+lines.show()
