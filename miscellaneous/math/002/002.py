a03b99c2cdf50e936c95f1337dd85800ecc6f25d
Bill Westfall <billwestfall@yahoo.com>
2023-07-17 12:07:53 -0700

    Update 002.py

--- miscellaneous/math/002/002.py
+++ miscellaneous/math/002/002.py
@@ -3,7 +3,9 @@ import numpy as np
 
 #fig = plt.figure() # manually create a figure
 #lines = plt.plot(x, y) # plot data
-
+def f(x):
+  return x*(x - 2)*np.exp(3 - x)
+  
 x = np.linspace(-0.5, 3.0)
 lines = plt.plot(x, f(x), x, x**2, x, 1 - x)
 
