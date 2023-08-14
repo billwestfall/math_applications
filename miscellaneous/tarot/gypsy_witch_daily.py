2917989a28eb9356532a0e7cc3388006f101428a
Bill Westfall <billwestfall@yahoo.com>
2023-08-14 08:38:17 -0700

    Create gypsy_witch_daily.py

---
+++ miscellaneous/tarot/gypsy_witch_daily.py
@@ -0,0 +1,11 @@
+import random
+
+card_list = ['Ace', 'Two', 'Three', 'Four']
+
+daily = random.sample(card_list, 3)
+
+print()
+print("First card is your past, second is now, third is your future:")
+print()
+print(daily)
+print()
