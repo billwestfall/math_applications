c8fe2a509d17281b10bc30e3fa0ef9a38cce0339
Bill Westfall <billwestfall@yahoo.com>
2023-10-22 17:46:51 -0700

    Update one_card.py

--- miscellaneous/tarot/one_card.py
+++ miscellaneous/tarot/one_card.py
@@ -1,3 +1,3 @@
 import random
 
-print(random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, "Page", "Knight", "Queen", "King", "Ace"]), random.choice(["Swords", "Cups", "Coins", "Wands"]), random.choice(["Natural", "Reversed"]) or random.choice(["Fool", "Magician", "High Priestess"]))
+print(random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, "Page", "Knight", "Queen", "King", "Ace"]), random.choice(["Swords", "Cups", "Coins", "Wands"]), random.choice(["Natural", "Reversed"]) or random.choice(["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant", "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", "Temperance", "The Devil", "The Tower", "The Star", "The Moon", "The Sun", "Judgement", "The World"]), random.choice(["Natural", "Reversed"])
