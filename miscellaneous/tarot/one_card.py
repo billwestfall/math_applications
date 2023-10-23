6e8a440ea9ba57fdff67b8ea0b01dd8df97bb5e1
Bill Westfall <billwestfall@yahoo.com>
2023-10-22 17:39:24 -0700

    Create one_card.py

---
+++ miscellaneous/tarot/one_card.py
@@ -0,0 +1,3 @@
+import random
+
+print random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, "Page", "Knight", "Queen", "King", "Ace"]), random.choice(["Swords", "Cups", "Coins", "Wands"]), random.choice(["Natural", "Reversed"]) or random.choice(["Fool", "Magician", "High Priestess"])
