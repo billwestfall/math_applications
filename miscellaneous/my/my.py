76efa373f535757fe811bef8a07d1f17502a3e3f
Bill Westfall <billwestfall@yahoo.com>
2024-06-09 21:44:21 -0700

    Update my.py

--- miscellaneous/my/my.py
+++ miscellaneous/my/my.py
@@ -15,7 +15,9 @@ class My:
         for line in self.code.strip().split('\n'):
             self.line_nr += 1
             for token in line.strip().split(' '):
-                if token == 'print':
+                if token == 'print(':
+                    yield (token,)
+                elif token == ')':
                     yield (token,)
                 elif token.isnumeric():
                     yield ('number', int(token))
@@ -60,7 +62,7 @@ class My:
 
     def parse_print_statement(self):
         token = self.next_token()
-        if token[0] != 'print':
+        if token[0] != 'print(':
             self.return_token(token)
             return False
         if not self.parse_expression():
